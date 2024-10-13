f0025d2867ad3058808e6f3409e12fa51cd3e0cb
cip2186 <cip2186@yahoo.it>
2024-10-13 16:40:09 +0200

    Update database.py

--- database.py
+++ database.py
@@ -18,6 +18,6 @@ class Database:
         acct = self.data.get(acct_id)
         if acct:
             bal = float(acct["due"]) - float(acct["paid"])
-            return f"US$ {bal:.2f}"
+            return f"$ {bal:.2f}"
             #return int(acct["due"]) - int(acct["paid"])
         return None
